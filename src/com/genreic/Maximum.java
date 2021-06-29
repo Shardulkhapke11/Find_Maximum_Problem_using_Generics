1db0dcac124f660ac7df152934f7629a36f450be
Shardulkhapke11 <shardulkhapke04@gmail.com>
2021-06-29 23:09:51 +0530

    Added: Refactor1

--- src/com/genreic/Maximum.java
+++ src/com/genreic/Maximum.java
@@ -6,42 +6,22 @@ public class Maximum {
         Integer a = 2, b = 4, c =6;
         Float x =3.4f, y = 4.5f, z = 6.7f;
         String l="apple",m="banana",n="orange";
-        FindMaximum (a,b,c);
-        FindMaximumF(x, y, z);
-        FindMaximumS(l, m, n);
-    }
 
-    public static void FindMaximumS(String a, String b, String c) {
-        String max = a;
+        Maximum.getMaximum(a, b, c);
+        Maximum.getMaximum(x, y, z);
+        Maximum.getMaximum(l, m, n);
+
+    }
+    private static<T extends Comparable> void getMaximum(T a, T b, T c ){
+        T max = a;
         if(b.compareTo(a)>0){
             max = b;
         }
         if(c.compareTo(max)>0){
             max = c;
         }
-        System.out.println("String Maximum Number is::"+max);
+        System.out.println("Maximum::"+max); max = a;
 
-    }
-    public static void FindMaximumF(Float a, Float b, Float c) {
-        Float max = a;
-        if(b.compareTo(a)>0){
-            max = b;
-        }
-        if(c.compareTo(max)>0){
-            max = c;
-        }
-        System.out.println("Float Maximum Number is::"+max);
-
-    }
-    private static void FindMaximum(Integer a, Integer b, Integer c) {
-        Integer max = a;
-        if(b.compareTo(a)>0){
-            max = b;
-        }
-        if(c.compareTo(max)>0){
-            max = c;
-        }
-        System.out.println("Integer Maximum Number is::"+max);
     }
 
 }
