ded02123d5507b2271b360a4fe1c546fc6b2ea3b
Shardulkhapke11 <shardulkhapke04@gmail.com>
2021-06-29 22:56:46 +0530

    Added: UC3 String Find maximum

--- README.md
+++
@@ -1,1 +0,0 @@
-Welcome to Generics Program
--- src/com/genreic/Maximum.java
+++ src/com/genreic/Maximum.java
@@ -5,11 +5,23 @@ public class Maximum {
         System.out.println("Welcome to Generic Program.");
         Integer a = 2, b = 4, c =6;
         Float x =3.4f, y = 4.5f, z = 6.7f;
+        String l="apple",m="banana",n="orange";
         FindMaximum (a,b,c);
-
         FindMaximumF(x, y, z);
+        FindMaximumS(l, m, n);
     }
 
+    public static void FindMaximumS(String a, String b, String c) {
+        String max = a;
+        if(b.compareTo(a)>0){
+            max = b;
+        }
+        if(c.compareTo(max)>0){
+            max = c;
+        }
+        System.out.println("String Maximum Number is::"+max);
+
+    }
     public static void FindMaximumF(Float a, Float b, Float c) {
         Float max = a;
         if(b.compareTo(a)>0){
@@ -31,4 +43,7 @@ public class Maximum {
         }
         System.out.println("Integer Maximum Number is::"+max);
     }
+
 }
+
+
